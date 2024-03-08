119c0028eca2ceadeb674216755aa467a16dabab
Roman kebede <romankebede69@gmail.com>
2024-03-08 15:48:13 +0000

    Update LAB-19-Error-Handling.dart

--- LAB-19-Error-Handling.dart
+++ LAB-19-Error-Handling.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 void main() {
   print("Enter a number: ");
-  String userInput = stdin.readLineSync();
+  String? userInput = stdin.readLineSync();
   
   try {
-    int number = int.parse(userInput);
-    print("The number is: $number");
+    if (userInput != null) {
+      int number = int.parse(userInput);
+      print("The number is: $number");
+    } else {
+      print("Invalid input! Please enter a valid number.");
+    }
   } catch (e) {
     print("Invalid input! Please enter a valid number.");
   }
